cc3913307df1212bb2705ffdb27ad7a07359a1f3
Surya <suryarathan005@gmail.com>
2025-11-19 03:11:33 +0530

    inital commit

---
+++ backend/com-sg-api-gateway/src/main/java/com/sg/api/gateway/filter/RequestLogFilter.java
@@ -0,0 +1,24 @@
+package com.sg.api.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class RequestLogFilter implements GlobalFilter, Ordered {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		System.out.println("Request Path: " + exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return -1; // highest priority
+	}
+}
